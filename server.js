86fd9f0ab255fffc92d566f9e2a0bc6ce966bc56
LOGANATHANAN <anloganathan20@gmail.com>
2020-12-22 09:20:39 +0530

    Pushing into repository

---
+++ server.js
@@ -0,0 +1,17 @@
+const puppeteer=require('puppeteer');
+
+async function scrape(url){
+    const browser=await puppeteer.launch({headless:false});
+    const page=await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector("span [title='Mohan(TPT)']");
+    const target=await page.$("span [title='Mohan(TPT)']");
+    await target.click();
+    const inp=await page.$("#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text");
+    for(let i=0;i<100;i++){
+        await inp.type("Oops! Something went wrong!");
+        await page.keyboard.press("Enter");
+    }
+}
+
+scrape("https://web.whatsapp.com");
